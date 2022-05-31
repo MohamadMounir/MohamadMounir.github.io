3cc598285a2276fafeae5d3ceabd953504ebab23
MohamadMounir <103706212+MohamadMounir@users.noreply.github.com>
2022-06-01 00:09:45 +0300

    new update for TaskListWebsite
    -Add feature to change color and be saved in LocalStorage.

--- TaskListWebsite/main.js
+++ TaskListWebsite/main.js
@@ -11,10 +11,6 @@ let mySubmit = document.querySelector("[type=submit]");
 
 ////---------for declare Task Shower 
 
-
-let TaskShower = document.createElement("div");
-TaskShower.className = "taskShower";
-document.body.appendChild(TaskShower);
 let myTaskShower = document.querySelector(".taskShower");
 
 
@@ -65,17 +61,25 @@ window.onload = function (){
 
 
 ////---------To re-write Task from storage
+let myTaskRe = /Task\d+/;
+
 window.onload = function (){
-    for(index=0; index<localStorage.getItem("myITNumber") + 1;index++){
-        if(localStorage.getItem(`Task${index}`) !== "" && localStorage.getItem(`Task${index}`) !== null){
-            createTextContent(localStorage.getItem(`Task${index}`), `Task${index}`);
-        }else{
-            continue;
+    for(index=0; index<localStorage.length;index++){
+        if(myTaskRe.test(localStorage.key(index))){
+            createTextContent(localStorage.getItem(localStorage.key(index)), `${localStorage.key(index)}`);
         }
     }
 }
 
 
+
+// for(i=0; i<localStorage.length;i++){
+
+//     console.log(myTaskRe.test(localStorage.key(i)));
+//     if(myTaskRe.test(localStorage.key(i))){
+//         console.log(localStorage.getItem(localStorage.key(i)));
+//     }
+// }
 
 
 
@@ -123,8 +127,179 @@ function deleteTool(){
         if(myButtonDelete[i] !== null){
             myButtonDelete[i].onclick = function (e){
                 localStorage.removeItem(`${e.target.parentElement.getAttribute("id")}`);
+                console.log(e.target.parentElement.getAttribute("id"))
                 e.target.parentElement.remove();
             }
         }
     }  
 }
+
+
+
+
+
+
+///////-------------------------[3]ChangeColor-----------------------------------------
+
+
+/////Color set
+
+let mainColorTheme = {
+    mainColor: `#ff4b27`,
+    secondColor: `#eeeeee`,
+    thirdColor: `white`,
+    textColor: `black`,
+    buttonTextColor: `white`,
+};
+
+///Default Theme 
+//////Not Yet...........................................
+
+let secondColorTheme = {
+    mainColor: `#003B46`,
+    secondColor: `#66A5AD`,
+    thirdColor: `#C4DFE6`,
+    textColor: `black`,
+    buttonTextColor: `white`,
+};
+let thirdColorTheme = {
+    mainColor: `#379683`,
+    secondColor: `#8ee4af`,
+    thirdColor: `#edf5e1`,
+    textColor: `black`,
+    buttonTextColor: `white`,
+};
+let forthColorTheme = {
+    mainColor: `#66fcf1`,
+    secondColor: `#45a29e`,
+    thirdColor: `#1f2833`,
+    textColor: `#c5c6c7`,
+    buttonTextColor: `white`,
+};
+let fivthColorTheme = {
+    mainColor: `#86c232`,
+    secondColor: `#61892f`,
+    thirdColor: `#222629`,
+    textColor: `white`,
+    buttonTextColor: `white`,
+
+};
+
+
+///////Re-set colors from localStorage
+
+window.addEventListener("load", function (){
+
+        document.styleSheets[0].cssRules[0].style.setProperty(`--mainColor`, `${localStorage.getItem("selectedMainColor")}`);
+        document.styleSheets[0].cssRules[0].style.setProperty(`--secondColor`, `${localStorage.getItem("selectedSecondColor")}`);
+        document.styleSheets[0].cssRules[0].style.setProperty(`--thirdColor`, `${localStorage.getItem("selectedThirdColor")}`);
+        document.styleSheets[0].cssRules[0].style.setProperty(`--textColor`, `${localStorage.getItem("selectedTextColor")}`);
+        document.styleSheets[0].cssRules[0].style.setProperty(`--buttonTextColor`, `${localStorage.getItem("selectedButtonTextColor")}`);
+
+    
+
+    })
+
+
+
+
+
+
+
+
+
+////Switch Function
+
+function clickedFun(){
+    ////get elements
+    let myColorSwitch = document.getElementsByClassName("colorR");
+
+    /////Changing color Fun
+    for(i=0; i<myColorSwitch.length; i++){
+        myColorSwitch.item(i).onclick = function (e){
+            switch(e.target.getAttribute("id")){
+                
+                case "mainColorTheme":
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--mainColor`, `${mainColorTheme.mainColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--secondColor`, `${mainColorTheme.secondColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--thirdColor`, `${mainColorTheme.thirdColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--textColor`, `${mainColorTheme.textColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--buttonTextColor`, `${mainColorTheme.buttonTextColor}`);
+                    break;
+                case "secondColorTheme": 
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--mainColor`, `${secondColorTheme.mainColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--secondColor`, `${secondColorTheme.secondColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--thirdColor`, `${secondColorTheme.thirdColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--textColor`, `${secondColorTheme.textColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--buttonTextColor`, `${secondColorTheme.buttonTextColor}`);
+                    break;
+                case "thirdColorTheme": 
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--mainColor`, `${thirdColorTheme.mainColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--secondColor`, `${thirdColorTheme.secondColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--thirdColor`, `${thirdColorTheme.thirdColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--textColor`, `${thirdColorTheme.textColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--buttonTextColor`, `${thirdColorTheme.buttonTextColor}`);
+                    break;
+                case "forthColorTheme": 
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--mainColor`, `${forthColorTheme.mainColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--secondColor`, `${forthColorTheme.secondColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--thirdColor`, `${forthColorTheme.thirdColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--textColor`, `${forthColorTheme.textColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--buttonTextColor`, `${forthColorTheme.buttonTextColor}`);
+                    break;
+                case "fivthColorTheme": 
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--mainColor`, `${fivthColorTheme.mainColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--secondColor`, `${fivthColorTheme.secondColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--thirdColor`, `${fivthColorTheme.thirdColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--textColor`, `${fivthColorTheme.textColor}`);
+                    document.styleSheets[0].cssRules[0].style.setProperty(`--buttonTextColor`, `${fivthColorTheme.buttonTextColor}`);
+                    break;
+
+
+            }
+
+                ////set in localStorage
+            let rootValues = getComputedStyle(document.documentElement);
+
+            localStorage.setItem("selectedMainColor", rootValues.getPropertyValue("--mainColor"));
+            localStorage.setItem("selectedSecondColor", rootValues.getPropertyValue("--secondColor"));
+            localStorage.setItem("selectedThirdColor", rootValues.getPropertyValue("--thirdColor"));
+            localStorage.setItem("selectedTextColor", rootValues.getPropertyValue("--textColor"));
+            localStorage.setItem("selectedButtonTextColor", rootValues.getPropertyValue("--buttonTextColor"));
+        
+        }
+    } 
+}
+
+clickedFun();
+
+////Function check and return colorTheme type
+
+
+/////SetColor 
+
+// document.styleSheets[0].cssRules[1].style.setProperty(`--mainColor`, `#ff4b27`);
+// document.styleSheets[0].cssRules[1].style.setProperty(`--secondColor`, `#eeeeee`);
+// document.styleSheets[0].cssRules[1].style.setProperty(`--thirdColor`, `white`);
+// document.styleSheets[0].cssRules[1].style.setProperty(`--textColor`, `black`);
+// document.styleSheets[0].cssRules[1].style.setProperty(`--buttonTextColor`, `white`);
+
+///what I did?
+let rootValues = getComputedStyle(document.documentElement);
+console.log(rootValues.getPropertyValue("--mainColor"));
+
+
+
+
+
+/////RegulareExpression
+
+// let myTaskRe = /Task\d+/;
+// for(i=0; i<localStorage.length;i++){
+
+//     console.log(myTaskRe.test(localStorage.key(i)));
+//     if(myTaskRe.test(localStorage.key(i))){
+//         console.log(localStorage.getItem(localStorage.key(i)));
+//     }
+// }
+// console.log(localStorage.key(0));
