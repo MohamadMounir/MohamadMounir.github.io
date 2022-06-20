794b4d1fd44c6460cec927cee98333afd8cf48b2
MohamadMounir <103706212+MohamadMounir@users.noreply.github.com>
2022-06-20 14:13:04 +0300

    Update for ForthWebsite
    Make the Arrows in main div working

--- ForthWebsite/main.js
+++ ForthWebsite/main.js
@@ -1,4 +1,6 @@
 
+
+/////Make Menu Functionable
 let MyMenuButton = document.getElementById("myMenu");
 
 MyMenuButton.onclick = function (){
@@ -12,3 +14,64 @@ MyMenuButton.onclick = function (){
     document.querySelector("nav .Container").classList.toggle("MenuContainerEnable");
     document.querySelector(".Navigation").classList.toggle("Show");
 }
+
+
+
+
+
+
+
+
+
+
+/////Make the Arrow im main div work
+let myPages = document.querySelectorAll("main .Container > div");
+let myArrows = document.querySelectorAll(".Arrow > span");
+myArrows[0].addEventListener("click", arrowButton);
+myArrows[1].addEventListener("click", arrowButton);
+
+function arrowButton(){
+
+    /////Nested Function
+    function checkDisplayBlock(arr){
+        for(i=0; i<arr.length; i++){
+            if(getComputedStyle(arr[i]).display === "block"){
+                return arr[i];
+            }else if(arr[i].style.display === "block"){
+                return arr[i];
+            }
+        }
+    }
+    function displayNone(arr){
+        arr.style.display = "none";
+    }
+
+    /////Main Function
+
+    let currnetPage = checkDisplayBlock(myPages);
+    if(this.classList.item(0) === "right"){
+        if(currnetPage.nextElementSibling === null){
+            
+            myPages.forEach(displayNone);
+            myPages[0].style.display = "block";
+        }else if(currnetPage.nextElementSibling !== null){
+            myPages.forEach(displayNone);
+            currnetPage.nextElementSibling.style.display = "block";
+        }
+    }else if(this.classList.item(0) === "left"){
+        if(currnetPage.previousElementSibling === null){
+            
+            myPages.forEach(displayNone);
+            myPages[2].style.display = "block";
+        }else if(currnetPage.previousElementSibling !== null){
+            myPages.forEach(displayNone);
+            currnetPage.previousElementSibling.style.display = "block";
+        }
+    }
+
+
+}
+
+
+
+
