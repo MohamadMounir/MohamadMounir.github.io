d77b59f34f47f25026cfd5ec5ab9d6474c782f8b
MohamadMounir <103706212+MohamadMounir@users.noreply.github.com>
2022-08-08 22:45:58 +0300

    Add "ThirdTemplate"

---
+++ Elzero Tutorial - template 3/main.js
@@ -0,0 +1,7 @@
+let mainArrow = document.getElementById("mainArrow")
+
+let articlesPage = document.getElementById("articles")
+
+mainArrow.onclick = function (){
+    articlesPage.scrollIntoView()
+}
